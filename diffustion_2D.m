e5e3c80c308ef6d7a1266b69a0b1bf6830465649
bhargavNS0 <161283649+bhargavNS0@users.noreply.github.com>
2024-04-15 20:00:20 +0530

    Update diffustion_2D.m

--- diffustion_2D.m
+++ diffustion_2D.m
@@ -2,41 +2,66 @@
 clear
 clc
 tic
+k = 60.5;             % thermal conductivity in W/mK
+q = 1000;             % Heat generation per unit volume W/cubic m 
+L = 1;                % Half of total edge Length, taken equal lengths in both directions
 
+% ---------------Grid generation---------------
 m = input("Enter the number of elements in one direction: "); % taking same no. of elements in both directions
 dx = 1/m;
 dx2 = dx^2;
+
+%---------------Discretization----------------
 a = ones(1,m)*4;
 b = ones(1,m)*-1;
 b(1) = -2;
 b(m) = 0;
 c = ones(1,m)*-1;
 c(1) = 0;
-theta = zeros(m*(m+1),1);
 d = ones(1,m)*dx2;
 
-[T,x] = LineByLineTDMAfunc(a,b,c,d,m);
+%----------------Processing-------------------
+theta = LineByLineTDMAfunc(a,b,c,d,m);
+theta = theta*q*L^2/k + 25;             % surrounding temperature is 25 celsius
+theta = transpose(theta);
+theta = reshape(theta,m,m+1);
+T = ones(m+1)*25;
+T(1:m,:) = theta;
 
+%---------------Post processing--------------
+figure;
 axis('equal')
 hold on
-X = linspace(0,1,m+1);
-Y = linspace(0,1,m+1);
+xlim([-L,L])
+ylim([-L,L])
+X = linspace(0,L,m+1);
+Y = linspace(0,L,m+1);
 surface(X,Y,T)
 
-X = linspace(0,-1,m+1);
-Y = linspace(0,1,m+1);
+X = linspace(0,-L,m+1);
+Y = linspace(0,L,m+1);
 surface(X,Y,T)
 
-X = linspace(0,1,m+1);
-Y = linspace(0,-1,m+1);
+X = linspace(0,L,m+1);
+Y = linspace(0,-L,m+1);
 surface(X,Y,T)
 
-X = linspace(0,-1,m+1);
-Y = linspace(0,-1,m+1);
+X = linspace(0,-L,m+1);
+Y = linspace(0,-L,m+1);
 surface(X,Y,T)
-xlabel('x/L')
-ylabel('y/L')
+colorbar
+xlabel('x')
+ylabel('y')
 zlabel('Temperature [°C]')
 hold off
+
+figure;
+X = linspace(0,L,m+1);
+Y = linspace(0,L,m+1);
+colormap("hot")
+contourf(X,Y,T)
+colorbar;
 toc
+
+
 
